1e3993f40a18bf745b075e723b7ed843904bd76a
Nazmul177 <nazmul177382@gmail.com>
2023-01-30 20:29:49 +0600

    Project on webpack and Babel

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./app.scss";
+import { Modal } from "bootstrap";
+import blogImage from "./logo.png";
+
+document.querySelector('.img-1').setAttribute('src', blogImage);
